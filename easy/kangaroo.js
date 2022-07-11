e01bb5c39f709448b74a6861b9483db22f0f4f12
Abir-Bouhriz <abirbouhrizdaidj@gmail.com>
2022-07-11 13:41:27 +0100

    kangaroo solution

--- easy/kangaroo.js
+++ easy/kangaroo.js
@@ -1,7 +1,7 @@
 function kangaroo(x1, v1, x2, v2) {
     if (v1 <= v2 && x2 > x1) {
         return "NO";
-    }else if((x2 - x1) % (v2 - v1) == 0){//x1+(n*v1)=x2+(n*v2)  donc arrive at the same time
+    }else if((x2 - x1) % (v2 - v1) == 0){
         return "YES" ;
     }else{
         return "NO";
